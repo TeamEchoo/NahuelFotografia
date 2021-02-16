63c69567ad2f16bbd94fbfd5b04f982d2f54ab8e
Laura <laura.rodcarrrion@gmail.com>
2021-02-16 19:56:22 +0100

    first commit

---
+++ routes/web.php
@@ -0,0 +1,27 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\ImageController;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+
+
+Route::get('/photo', [ImageController::class, 'create'])->name('photo.create');
+Route::post('/photo',[ImageController::class, 'store'])->name('photo.store');
+Route::get('/photo/{$id}',[ImageController::class, 'edit'])->name('photo.edit');
+Route::put('/photo/{$id}',[ImageController::class,'update'])->name('photo.update');
+Route::delete('/photo/{$id}',[ImageController::class,'delete'])->name('photo.delete');
