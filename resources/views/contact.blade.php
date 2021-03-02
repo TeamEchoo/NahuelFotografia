3f52657c8794daf213c0e0cd964c3f32bb3245fe
JosephCrespin <67466171+JosephCrespin@users.noreply.github.com>
2021-03-02 09:49:40 +0100

    contact check

--- resources/views/contact.blade.php
+++ resources/views/contact.blade.php
@@ -1,55 +1,61 @@
 @extends('layouts.app')
 @section('content')
-    <section id="contact" class="parallax-section">
-        <div class="container">
-            <div class="row">
-                <div class="col-md-5 col-sm-8">
-                    <!-- CONTACT INFO -->
-                    <div id="paddingContactImage" class="wow fadeInUp contact-info" data-wow-delay="0.4s">
-                        <div class="section-title">
-                            <img src="https://d2s22rn0thm1js.cloudfront.net/bT19YiUmtg7z-v0EcpAyidRkQqU=/958x1440/smart/http%3A%2F%2Fd2hl4mfiesch9e.cloudfront.net%2Ffreedombmx%2Fwp-content%2Fuploads%2F2019%2F07%2Fnahuel-martinez-photographer.jpg" width="70%">
-                        </div>
-                        <div id=PaddingDescription>
-                            <p><i class="fa fa-map-marker"></i>Nahuel Martinez</p>
-                            <p><i class="fa fa-comment"></i> <a href="mailto:info@company.com">nahuel@nahuelmartinez</a></p>
-                            <p><i class="fa fa-phone"></i>+49 (0)1637000359</p>
-                            <a href="https://www.instagram.com/nahhuelmarttinezphoto/"><img src="https://image.flaticon.com/icons/png/512/87/87390.png" width="10%"></a>
-                            <a href="https://www.instagram.com/nahhuelmarttinezphoto/"><img src="https://www.flaticon.es/svg/vstatic/svg/20/20673.svg?token=exp=1614154193~hmac=c339a2877ed73f4579d8cfbfe7d0df06" width="10%"></a>
-                        </div>
+<link href="{{ asset('css/contactMail.css') }}" rel="stylesheet" type="text/css">
+<section id="contact" class="parallax-section">
+    <div class="container">
+        <div class="row">
+            <div class="col-md-5 col-sm-8">
+                <!-- CONTACT INFO -->
+                <div id="paddingContactImage" class="wow fadeInUp contact-info" data-wow-delay="0.4s">
+                    <div class="section-title">
+                        <img src="https://d2s22rn0thm1js.cloudfront.net/bT19YiUmtg7z-v0EcpAyidRkQqU=/958x1440/smart/http%3A%2F%2Fd2hl4mfiesch9e.cloudfront.net%2Ffreedombmx%2Fwp-content%2Fuploads%2F2019%2F07%2Fnahuel-martinez-photographer.jpg" width="70%">
+                    </div>
+                    <div id=PaddingDescription>
+                        <p><i class="fa fa-map-marker"></i>Nahuel Martinez</p>
+                        <p><i class="fa fa-comment"></i> <a href="mailto:info@company.com">nahuel@nahuelmartinez</a></p>
+                        <p><i class="fa fa-phone"></i>+49 (0)1637000359</p>
+                        <a href="https://www.instagram.com/nahhuelmarttinezphoto/"><img src="https://image.flaticon.com/icons/png/512/87/87390.png" width="10%"></a>
+
                     </div>
                 </div>
-
-
-                <div id="paddingContact" class="col-md-7 col-sm-10">
-
-
-                    <!-- CONTACT FORM HERE -->
-                    <div class="wow fadeInUp" data-wow-delay="0.4s">
-                        <form id="contact-form" action="#" method="POST" action="{{ route('contact')}}">
-                            {{ csrf_field() }} 
-                            <!-- verificacion para ataques de identidad (419) -->
-
-                            <div class="col-md-6 col-sm-6">
-                                <input type="text" class="form-control" name="name" placeholder="Name" required="">
-                            </div>
-                            <div class="col-md-6 col-sm-6">
-                                <input type="email" class="form-control" name="email" placeholder="Email" required="">
-                            </div>
-                            <div class="col-md-12 col-sm-12">
-                                <textarea class="form-control" rows="5" name="message" placeholder="Message" required=""></textarea>
-                            </div>
-                            <div class="col-md-offset-8 col-md-4 col-sm-offset-6 col-sm-6">
-                                <button id="submit" type="submit" class="form-control" name="submit">Send</button>
-                            </div>
-                        </form>
-                    </div>
-                </div>
-
-
-
             </div>
+
+
+            <div id="paddingContact" class="col-md-7 col-sm-10">
+
+
+                <!-- CONTACT FORM HERE -->
+                <div class="wow fadeInUp" data-wow-delay="0.4s">
+                    <form id="contact-form" action="#" method="POST" action="{{ route('contact')}}">
+                        {{ csrf_field() }}
+                        <!-- verificacion para ataques de identidad (419) -->
+
+                        <div class="col-md-6 col-sm-6">
+                            <input type="text" class="form-control" name="name" placeholder="Name" required="">
+                        </div>
+                        <div class="col-md-6 col-sm-6">
+                            <input type="email" class="form-control" name="email" placeholder="Email" required="">
+                        </div>
+                        <div class="col-md-12 col-sm-12">
+                            <textarea class="form-control" rows="5" name="message" placeholder="Message" required=""></textarea>
+                        </div>
+                        <div class="col-md-offset-8 col-md-4 col-sm-offset-6 col-sm-6">
+                            <button id="submit" type="submit" class="form-control" name="submit">Send</button>
+                        </div>
+                        @if (session()->has('flash'))
+                            <div class="container">
+                                <div class="alert alert-success">{{ session ('flash') }}</div>
+                            </div>
+                        @endif
+                    </form>
+                </div>
+            </div>
+
+
+
         </div>
-    </section>
+    </div>
+</section>
 
 
 
