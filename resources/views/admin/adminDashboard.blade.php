b6f2a30a02621e7d001e867ffb41a7168c83f85d
JosephCrespin <67466171+JosephCrespin@users.noreply.github.com>
2021-03-04 09:59:41 +0100

    dashboard done

--- resources/views/admin/adminDashboard.blade.php
+++ resources/views/admin/adminDashboard.blade.php
@@ -6,9 +6,9 @@
     <div class="container">
         <div class="row">
             <div class="col">
-                <table class="table">
+                <table class="tablee">
                     <thead>
-                        <tr>
+                        <tr class="tr">
                             <th>Title</th>
                             <th>Category</th>
                             <th>Cover image</th>
@@ -21,25 +21,21 @@
                         <td> {{ $album->title }} </td>
                         <td> {{ $album->category }} </td>
                         <td></td>
-
                         <td>
                             <a href="{{ route('albumEdit', $album->id) }}">
-                                <i class="far fa-edit"></i>
+                                <button type="submit"> <i class="far fa-edit"></i>
                             </a>
-                        </td>
-
-                        <td>
                             <form method='POST' action=" {{ route( 'albumDelete', $album->id) }}">
                                 @csrf
                                 @method('DELETE')
-                                <button type="submit"><i class="far fa-trash-alt"></i></input>
+                                <button type="submit"><i class="fas fa-trash-alt"></i></input>
                             </form>
                         </td>
                     </tr>
                     @endforeach
                 </table>
 
-                <a href="{{ route('albumCreate') }}">New Album</a>
+                <a id="newAlbum" href="{{ route('albumCreate') }}">New Album</a>
 
             </div>
         </div>
