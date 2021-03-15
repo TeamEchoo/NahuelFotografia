6ea3b9a4ef856158004a42372ea6da602b0b186d
Moi <mmena4@hotmail.com>
2021-03-15 13:07:24 +0100

    css updated

--- resources/views/admin/editPhoto.blade.php
+++ resources/views/admin/editPhoto.blade.php
@@ -1,42 +1,71 @@
 <x-head>
 <x-logoAdmin />
 
-<form action=" {{ route( 'photoEdit', $photo->id ) }}"  method="POST">
+<div class="createphoto-container">
+    <h1>Edit Photo</h1>
+
+    <form class="form2" action=" {{ route( 'photoEdit', $photo->id ) }}"  method="POST">
+            
+            @method('PUT')
+            @csrf
+
+
+        <div class="inputsGeneral">
+            <div class="inputs-container">
+                <div class="inputs">
+                    <label for="title">Title:</label>
+                    <input name="title" type="text" value="{{ $photo->title }}">
+                </div>    
+                
+                <div class="inputs">
+                    <label for="epigraph">Epigraph:</label>
+                    <input name="epigraph" type="text" value="{{ $photo->epigraph }}">
+                </div>
+
+                <div class="inputs">
+                    <label for="person">Person:</label>
+                    <input name="person" type="text" value="{{ $photo->person }}">
+                </div>
+                    
+                <div class="inputs">
+                    <label for="link">Link:</label>
+                    <input name="link" type="text" value="{{ $photo->link }}">
+                </div>
+
+                <div class="inputs">
+                    <label for="filename">Photo:</label>
+                    <input  type="file" style="width: 172px"  name="filename" accept="image/*" class="form-control">
+                </div>
+            </div>   
         
-        @method('PUT')
-        @csrf
 
-        <label for="title">Title:</label>
-        <input name="title" type="text" value="{{ $photo->title }}">
+
+
+
+            <div id="picture-uploaded1">   
+                <label for="thumbnail">Actual Picture</label>
+                <img src="{{$photo->filename}}" >
+            </div>    
+        </div>
+
+
+            <div class="buttons2">  
+                <label for="cover_image">Cover:</label>
+                <input name="cover_image" type="checkbox">
         
-        <label for="epigraph">Epigraph:</label>
-        <input name="epigraph" type="text" value="{{ $photo->epigraph }}">
+            
+                <a href="{{ route( 'photoUpdate', $photo->id) }}">
+                <input type="submit" value="Save">
+                </a>
 
-
-        <label for="person">Person:</label>
-        <input name="person" type="text" value="{{ $photo->person }}">
-        
-        <label for="link">Link:</label>
-        <input name="link" type="text" value="{{ $photo->link }}">
-
-        <label for="filename">Photo:</label>
-        <input  type="file" style="width: 172px"  name="filename" accept="image/*" class="form-control">
-
-        <label for="thumbnail">Actual Picture</label>
-        <img src="{{$photo->filename}}" >
-
-        <label for="cover_image">Cover:</label>
-        <input name="cover_image" type="checkbox">
-        
-        
-        <a href="{{ route( 'photoUpdate', $photo->id) }}">
-        <input type="submit" value="Save">
-        </a>
+                <a href="{{ route ('albumEdit', $photo->album_id)}}">
+                <button >Cancel</button>
+                </a>
+            </div>    
+ 
 
     </form>
-    
-        <a href="{{ route ('albumEdit', $photo->album_id)}}">
-            <button >Cancel</button>
-        </a>
+</div>    
+
 
 </x-head>
