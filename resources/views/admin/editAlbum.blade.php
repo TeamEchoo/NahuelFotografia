6841dd609d789325fcae3264087af4ad3af4162d
Moi <mmena4@hotmail.com>
2021-03-15 14:56:27 +0100

    dashboard updated

--- resources/views/admin/editAlbum.blade.php
+++ resources/views/admin/editAlbum.blade.php
@@ -4,44 +4,29 @@
 
 
     <div class='wrapper'>
-        <div class='row'>
-            <div class="column">
                 <div class='title-column'>
-                    <form action="{{ route('albumUpdate', $album->id) }}" method="POST">
-
+                     <form class="titleTwo" action="{{ route('albumUpdate', $album->id) }}" method="POST">
                         @method("PUT")
                         @csrf
 
-                        <h2 class="categoryTitleOne" for="title">Title:</h2>
+                        <h2 class="categoryTitleOne1" for="title">Title:</h2>
                         <input class="inputTitle" name="title" type="text" value="{{$album->title}}">
-                </div>
-            </div>
-            <div class='column'>
-                <div class='category-column'>
-
-                    <div class='column'>
-                <div class='category-column'>
-                    <h2 class="categoryTitle" for="category">Category:</h2>
-
-                    <select class="categorySelect" name="category" id="category">
-                        <option value="{{$album->category}}">{{$album->category}}</option>
-                        <option value="Overview">Overview</option>
-                        <option value="Editorial">Editorial</option>
-                        <option value="More">More</option>
-                    </select>
-                   
+                    </form>   
                 </div>
 
-                    
+                <div class='column'>
+                            <h2 class="categoryTitle" for="category">Category:</h2>
+
+                            <select class="categorySelect1" name="category" id="category">
+                                <option value="{{$album->category}}">{{$album->category}}</option>
+                                <option value="Overview">Overview</option>
+                                <option value="Editorial">Editorial</option>
+                                <option value="More">More</option>
+                            </select>
                 </div>
-            </div>
-        </div>
     </div>
-    <a  href="{{ route( 'albumUpdate', $album->id ) }}">
-        <button class="saveButton" class="cancelButton" type="submit">Save</button>
-    </a>
- 
-    </form>
+
+
 
     @if(!$photos)
     <h1>Upload photos!</h1>
@@ -98,9 +83,10 @@
     
 
     <div class="downButtons">
-
         <a class="addButton" href="{{ route('photoCreate', $album->id) }}">Add new Photo</a>
         <a class="cancelButton" href="{{ route('album') }}">Cancel</a>
+        <a  href="{{ route( 'albumUpdate', $album->id ) }}">
+        <a class="saveButton" class="cancelButton" type="submit">Save</a>
     </div>
    
 </x-head>
