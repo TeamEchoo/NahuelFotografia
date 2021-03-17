dae251c8dc94b601a11cfe1f52b973b51b80ba67
Moi <mmena4@hotmail.com>
2021-03-17 10:06:55 +0100

    edit photo style updated

--- resources/views/admin/editPhoto.blade.php
+++ resources/views/admin/editPhoto.blade.php
@@ -4,11 +4,11 @@
 @if(session('success'))
         <div class="alert alert-success">
           {{ session('success') }}
-        </div> 
+        </div>
         @endif
 
-<form action=" {{ route( 'photoUpdate', $photo->id ) }}"  method="POST" enctype="multipart/form-data">
-        
+<form class="editPhotoForm" action=" {{ route( 'photoUpdate', $photo->id ) }}"  method="POST" enctype="multipart/form-data">
+
         @csrf
         @method('PUT')
 
@@ -18,8 +18,8 @@
                 <div class="inputs">
                     <label for="title">Title:</label>
                     <input name="title" type="text" value="{{ $photo->title }}">
-                </div>    
-                
+                </div>
+
                 <div class="inputs">
                     <label for="epigraph">Epigraph:</label>
                     <input name="epigraph" type="text" value="{{ $photo->epigraph }}">
@@ -29,7 +29,7 @@
                     <label for="person">Person:</label>
                     <input name="person" type="text" value="{{ $photo->person }}">
                 </div>
-                    
+
                 <div class="inputs">
                     <label for="link">Link:</label>
                     <input name="link" type="text" value="{{ $photo->link }}">
@@ -39,33 +39,32 @@
                     <label for="filename">Photo:</label>
                     <input  type="file" style="width: 172px"  name="filename" accept="image/*" class="form-control">
                 </div>
-            </div>  
-        
-        
+            </div>
 
-            <div id="picture-uploaded1">   
+
+
+            <div id="picture-uploadedEdit">
                 <label for="thumbnail">Actual Picture</label>
                 <img src="{{$photo->filename}}" >
-            </div>    
+            </div>
         </div>
 
 
-            <div class="buttons2">  
-            
+        <div class="buttons">
+
             <label for="cover_image">Cover:</label>
-            @if($photo->cover_image == true)
-                <input name="cover_image" type="checkbox" checked>
-            @else
-                <input name="cover_image" type="checkbox">
-            @endif
-            
-            </div>    
-            <input type="submit" value="Save">
-            <a href="{{ route ('albumEdit', $photo->album_id)}}">
-            <button >Cancel</button>
-                </a>
-        
-    </form>
+                @if($photo->cover_image == true)
+                    <input name="cover_image" type="checkbox" checked>
+                @else
+                    <input name="cover_image" type="checkbox">
+                @endif
+        </div>
+        <div class="buttons">
+                <input type="submit" value="Save">
+                <a href="{{ route ('albumEdit', $photo->album_id)}}">
+                <button >Cancel</button></a>
+        </div>
+</form>
 
 
 </x-head>
